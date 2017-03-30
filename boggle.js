c300d1f2215d8d24b322877fbcf98ea0d436d1b1
stedyyulius <stedyyulius@gmail.com>
2017-03-30 15:20:51 +0700

    first commit

---
+++ boggle.js
@@ -0,0 +1,13 @@
+function board(){
+  var box = [];
+  let huruf = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let random = huruf.charAt(Math.floor(Math.random() * 26))
+for (let i = 0; i < 4; i++){
+  box.push([])
+  for (let j = 0; j < 4; j++){
+  box[i].push(huruf.charAt(Math.floor(Math.random() * 26)))
+} }
+
+console.log(box);
+  }
+  board();
