3427aeff07f730d85d4cb86ee4b2d274275adb3d
fearzen3 <ivanhabi2@gmail.com>
2017-03-31 10:15:00 +0700

    revisi1

--- boggle.js
+++ boggle.js
@@ -1,26 +1,26 @@
 "use strict"
-
-function shake(num){
-  let char = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-  let boggle =[];
-  let board=[];
-  let size=num*num;
-  let index=0;
-  for(let i=0;i<size;i++){
-    index =Math.floor((Math.random() * 26));
-    boggle.push(char[index]);
+class Boogle {
+  constructor() {
+    this.char = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    this.board=[];
   }
-
-  for(let i = 0;i <=size-(num-1) ;i=i+num ){
-    board.push(boggle.slice(i,(i+num)));
+  shake(num){
+    let boggle =[];
+    let size=num*num;
+    let index=0;
+    for(let i=0;i<size;i++){
+      index =Math.floor((Math.random() * 26));
+      boggle.push(this.char[index]);
+    }
+    for(let i = 0;i <=size-(num-1) ;i=i+num ){
+      this.board.push(boggle.slice(i,(i+num)));
+    }
+    console.log(this.board);
   }
-
-
-
-
-  console.log(board);
-
 
 }
 
-shake(4);
+let Boogle1 =new Boogle();
+
+
+Boogle1.shake(4);
