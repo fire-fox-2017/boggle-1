d42a3d5af42809a51a35076cd1e75ea43b0d36cd
Matt Lee <email": null>
2017-03-31 14:36:01 +0700

    boggle 1 mattlcy

---
+++ boggle.js
@@ -0,0 +1,13 @@
+function shake(value) {
+  let board = []
+  let alphabets = "ABCDEFGHIJKLMNOPQRSTUVXYZ"
+
+  for (var i=0; i<value; i++) {
+    board[i] = []
+    for (var j=0; j<value; j++) {
+      board[i][j] = alphabets[Math.floor(Math.random()*10)]
+    }
+  }
+  return board
+}
+console.log(shake(3))
