9862123cfd296497d4c525f90d99ed24d855215b
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-03-31 16:18:36 +0800

    print in 2D format

--- boggle.js
+++ boggle.js
@@ -29,10 +29,29 @@ class BoogleBoard {
 
     console.log(board);
   }
+
+  printBoard2D() {
+    let board = "[";
+    for (let i = 0 ; i < this._dimension ; i++) {
+      board += "["
+      for (let j = 0 ; j < this._dimension ; j++) {
+        board += "'" +this._board[(i*this._dimension)+j] + "'";
+        if( j != this._dimension-1)
+          board += ",";
+      }
+      board += "]";
+      if (i != this._dimension-1)
+        board += "\n";
+    }
+    board += "]";
+
+    console.log(board);
+  }
+
 }
 
 
 let boogle = new BoogleBoard();
 
 boogle.shake(4);
-boogle.printBoard();
+boogle.printBoard2D();
